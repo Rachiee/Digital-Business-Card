a9144b462fb11c8c39d99fd1e9349e96e6d94a08
Rachiee <rachi7ace@gmail.com>
2025-11-22 10:12:15 +0100

    edited img srcs

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,9 +1,13 @@
+import twitterimg from "/images/Twitter.png"
+import igimg from "/images/Instagram.png"
+import gitimg from "/images/GitHub.png"
+
 export default function Footer() {
     return (
         <footer>
-            <img src="/images/Twitter.png"/>
-            <img src="/images/Instagram.png"/>
-            <img src="/images/GitHub.png"/>
+            <img src={twitterimg}/>
+            <img src={igimg}/>
+            <img src={gitimg}/>
         </footer>     
     )
 }
--- components/Info.jsx
+++ components/Info.jsx
@@ -1,17 +1,22 @@
+import aviimg from "/images/rachiavi.jpg"
+import mailimg from "/images/Mail.png"
+import linkedinimg from "/images/linkedin.png"
+
+
 export default function Info() {
     return(
          <header>
-            <img src="/images/rachiavi.jpg" className="main-img"/>
+            <img src={aviimg} className="main-img"/>
             <h1>Nara Nwaigbo</h1>
             <p>Frontend Developer</p>
             <a href="https://raxelle.framer.website/">raxelle.framer.website</a>
             <div className="links">
                 <div className="mail-link">
-                    <img src="/images/Mail.png"/>
+                    <img src={mailimg}/>
                     <span><a href="mailto:narachim.n@gmail.com" className="btn">Email</a></span>
                 </div>
                 <div className="lin-link">
-                    <img src="/images/linkedin.png"/>
+                    <img src={linkedinimg}/>
                     <span><a href="www.linkedin.com/in/narachim-nwaigbo-md-90608623a" className="btn">LinkedIn</a></span>
                 </div>
             </div>
